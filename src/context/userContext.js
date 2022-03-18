791e9dee94c90c9bc48297d5ed1d70f0295bc10e
Christian-pprogrammer <mpanoc6@gmail.com>
2022-03-18 07:12:46 +0200

    user context

--- src/context/userContext.js
+++ src/context/userContext.js
@@ -1,11 +1,20 @@
-import { createContext } from "react";
-import React from 'react'
+import { createContext, useEffect, useState } from "react";
 
 export const userContext = createContext();
 
 function UserProvider({children}) {
+  const [isLoggedIn, setIsLoggedIn]  = useState(false);
+  const setLoggedIn = (token) => {
+    localStorage.setItem('token', token);
+    setIsLoggedIn(true);
+  }
+  useEffect(()=>{
+    if(localStorage.getItem('token')) {
+        setIsLoggedIn(true);
+    }
+  },[]);
   return (
-    <userContext.Provider>
+    <userContext.Provider value={{isLoggedIn, setLoggedIn}}  >
         {children}
     </userContext.Provider>
   )
