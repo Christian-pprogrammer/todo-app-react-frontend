722deb97d02c4de41f3bece1b997ec7320f63e9b
Christian-pprogrammer <mpanoc6@gmail.com>
2022-03-18 07:12:27 +0200

    set navbar class to className to resolve error

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,31 +1,37 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { NavLink } from 'react-router-dom'
-
+import { userContext } from '../context/userContext';
 function Navbar() {
+  const {isLoggedIn} = useContext(userContext)
   return (
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
         <div className="container">
             <NavLink className="navbar-brand" to="/">Navbar</NavLink>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
             </button>
-            <div class="collapse navbar-collapse" id="navbarNav">
-                <ul class="navbar-nav ml-auto">
-                    <li class="nav-item active">
-                        <NavLink to="/" className="nav-link" >Home</NavLink>
+            <div className="collapse navbar-collapse" id="navbarNav">
+                <ul className="navbar-nav ml-auto">
+                    <li className="nav-item active">
+                        <NavLink to="/home" className="nav-link" >Home</NavLink>
                     </li>
-                    <li class="nav-item">
+                    <li className="nav-item">
                         <NavLink to="/todos" className="nav-link" >Todos</NavLink>
                     </li>
-                    <li class="nav-item">
-                        <NavLink to="/login" className="nav-link" >Login</NavLink>
-                    </li>
-                    <li class="nav-item">
-                        <NavLink to="/signup" className="nav-link" >Signup</NavLink>
-                    </li>
-                    <li class="nav-item">
-                        <NavLink to="/logout" className="nav-link" >Logout</NavLink>
-                    </li>
+                    {isLoggedIn ? (
+                        <li className="nav-item">
+                            <NavLink to="/logout" className="nav-link" >Logout</NavLink>
+                        </li>
+                    ):(
+                        <>
+                            <li className="nav-item">
+                                <NavLink to="/signup" className="nav-link" >Signup</NavLink>
+                            </li>
+                            <li className="nav-item">
+                                <NavLink to="/login" className="nav-link" >Login</NavLink>
+                            </li>
+                        </>
+                    )}
                 </ul>
             </div>
         </div>
