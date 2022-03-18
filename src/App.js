ee2ae105a58771cd2160b2271248288d8628e7c1
Christian-pprogrammer <mpanoc6@gmail.com>
2022-03-18 07:13:21 +0200

    include the context api in app.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,28 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import Signup from './components/Signup';
 import Navbar from './components/Navbar';
 import Login from './components/Login';
+import Home from './components/Home';
+import Todos from './components/Todos';
+import TodoItem from './components/TodoItem';
+import UserProvider from './context/userContext';
 
 function App() {
   return (
-    <Router>
-      <Navbar />
-      <Routes>
-        <Route path='/signup' element={<Signup />} />
-        <Route path='/login' element={<Login />} />
-      </Routes>
+    <UserProvider>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<Navigate to='/home' />} />
+          <Route path='/home' element={<Home />} />
+          <Route path='/todos' element={<Todos />} />
+          <Route path='/todo/:id' element={<TodoItem />} />
+          <Route path='/signup' element={<Signup />} />
+          <Route path='/login' element={<Login />} />
+        </Routes>
     </Router>
+    </UserProvider>
   );
 }
 
